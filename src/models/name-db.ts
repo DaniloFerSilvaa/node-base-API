cdc3e8508c8115e706462d86639838526957a6bc
Danilo Silva <58577886+DaniloFerSilvaa@users.noreply.github.com>
2022-12-26 03:18:08 -0300

    Update para deixar o model BD pré prontos

--- src/models/name-db.ts
+++ src/models/name-db.ts
@@ -1,2 +1,17 @@
 import { Model, DataType, DataTypes } from 'sequelize';
 import { sequelize } from '../instances/connect-db';
+
+export interface UserInstance extends Model {
+    id: number;
+}
+
+export const User = sequelize.define<UserInstance>('User', {
+    id: {
+        primaryKey: true,
+        autoIncrement: true,
+        type: DataTypes.INTEGER
+    }
+}, {
+    tableName: 'users',
+    timestamps: false
+});
